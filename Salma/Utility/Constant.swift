2bd4897ad9fc0c1b526ab8f1afbc1e2a9b18b890
martinmcspicy <gratianus.martin@pegipegi.com>
2022-04-12 00:30:26 +0700

    add colors asset, folder fix, uiview+extension

---
+++ Salma/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Salma
+//
+//  Created by Gratianus Martin on 4/12/22.
+//
+
+import Foundation
---
+++ Salma/Utility/Enum.swift
@@ -0,0 +1,8 @@
+//
+//  Enum.swift
+//  Salma
+//
+//  Created by Gratianus Martin on 4/12/22.
+//
+
+import Foundation

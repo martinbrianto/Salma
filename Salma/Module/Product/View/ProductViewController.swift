43af9fe15d716a523576a67934e4b0d3e7f0617c
sugiharso <sugiharsowijaya@gmail.com>
2022-04-21 21:35:46 +0700

    Add product-view and product-add-view
    product-view ui for empty state
    product-add-view ui

--- Salma/Module/Product/View/ProductViewController.swift
+++ Salma/Module/Product/View/ProductViewController.swift
@@ -15,15 +15,14 @@ class ProductViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+     override func viewWillDisappear(_ animated: Bool) {
+         self.navigationController?.setNavigationBarHidden(false, animated: animated);
+         super.viewWillDisappear(animated)
+     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         self.navigationController?.setNavigationBarHidden(true, animated: animated)
+     }
 
 }

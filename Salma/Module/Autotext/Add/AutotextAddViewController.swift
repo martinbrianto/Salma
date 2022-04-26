cc2f7083b7ddb7983e1c4140dd21b1505ca8419a
sugiharso <sugiharsowijaya@gmail.com>
2022-04-26 19:42:19 +0700

    Autotext
    Autotext view, no data found

---
+++ Salma/Module/Autotext/Add/AutotextAddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AutotextAddViewController.swift
+//  Salma
+//
+//  Created by Sugiharso Wijaya on 26/04/22.
+//
+
+import UIKit
+
+class AutotextAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Salma/Module/Autotext/Table/AutotextTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AutotextTableViewCell.swift
+//  Salma
+//
+//  Created by Sugiharso Wijaya on 26/04/22.
+//
+
+import UIKit
+
+class AutotextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
